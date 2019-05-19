42069e170bdfbdcfbdab7822d44e0c97b9fadf05
ayusintyaadewi <sintya_dewi2203@yahoo.com>
2019-05-19 21:27:24 +0800

    update data

--- app/src/main/java/com/example/diy/ProductData.java
+++ app/src/main/java/com/example/diy/ProductData.java
@@ -5,20 +5,75 @@ import java.util.Arrays;
 
 public class ProductData {
     public static String[][] data = new String[][]{
-            {"3","Kotak pensil botol bekas", "10 April 2019", String.valueOf(R.drawable.ee)},
-            {"6","Lampu Gantung Dari Sedotan", "13 April 2019", String.valueOf(R.drawable.aa)},
-            {"7","Pot Bunga Kotak Susu", "13 April 2019", String.valueOf(R.drawable.bb)},
-            {"5","Taplak meja sedotan", "12 April 2019", String.valueOf(R.drawable.cc)},
-            {"9","Hiasan lampu kardus", "12 April 2019", String.valueOf(R.drawable.dd)},
-            {"2","Kotak pensil botol bekas", "10 April 2019", String.valueOf(R.drawable.ee)},
-            {"1","Bingkai foto dari kardus","10 April 2019", String.valueOf(R.drawable.ff)}
+
+            {"5","Pot Gantung dari Kaleng Bekas", "19 April 2019", String.valueOf(R.drawable.oo)},
+            {"11","Tatakan Gelas dari Tutup Botol", "18 April 2019", String.valueOf(R.drawable.nn)},
+            {"8","Hiasan Batu Lukis", "17 April 2019", String.valueOf(R.drawable.kk)},
+            {"10","Hiasan Lampu dari Bambu", "16 April 2019", String.valueOf(R.drawable.jj)},
+            {"2","Tempat Lilin dari Bambu", "15 April 2019", String.valueOf(R.drawable.ii)},
+            {"3","Boneka dari Kain Flanel", "15 April 2019", String.valueOf(R.drawable.hh)},
+            {"4","Gantungan Kunci dari Kain Flanel", "14 April 2019", String.valueOf(R.drawable.gg)},
+            {"6","Lampu Gantung dari Sedotan", "13 April 2019", String.valueOf(R.drawable.aa)},
+            {"7","Pot Bunga dari Kotak Susu", "13 April 2019", String.valueOf(R.drawable.bb)},
+            {"5","Taplak meja dari Sedotan", "12 April 2019", String.valueOf(R.drawable.cc)},
+            {"9","Hiasan Lampu dari Kardus", "12 April 2019", String.valueOf(R.drawable.dd)},
+            {"2","Kotak Pensil dari Botol Bekas", "11 April 2019", String.valueOf(R.drawable.ee)},
+            {"2","Hiasan Lampu dari Kaleng Bekas", "18 April 2019", String.valueOf(R.drawable.mm)},
+            {"1","Keranjang dari Koran Bekas", "17 April 2019", String.valueOf(R.drawable.ll)},
+            {"1","Bingkai Foto dari Kardus","10 April 2019", String.valueOf(R.drawable.ff)}
     };
     public static String[][] step = new String[][]{
-            {"Potong bagian bawah dari dua botol plastik dengan gunting tajam."
-                    ,"Tempatkan ritsliting di bagian tepi atas salah satu botol dengan sisi tarik ritsliting menghadap keluar. Gunakan lem panas untuk merekatkannya di tempat. Terus lem sampai di ujung ritsleting."
-                    ,"Buka ritsleting sehingga Anda bisa merekatkan bagian botol lainnya."
-                    ,"Rekatkan sisi lain dari ritsleting di dalam botol ke dua menggunakan lem panas. Ingatlah untuk menyisakan setidaknya 1/8 ici dari tepi botol ke gigi ritsleting."
-                    ,"Sekarang kumpulkan semua barang yang berserakan dan masukkan masing-masing ke tempat khusus mereka sendiri. Anda akan sangat senang melakukannya."},
+
+            {"Siapkan alat dan bahan yang diperlukan."
+                    , "Cuci kaleng dengan air panas dicampur dengan baking soda."
+                    , "Cat kaleng dengan pilok untuk mendapatkan pot yang lebih cantik, atau pilih warna lain sesuai keinginan Anda."
+                    , "Pastikan cat disemprotkan secara rata dan biarkan mengering."
+                    , "Kemudian ikatkan tali pada kaleng. Setelah dua lingkar, ikat tali dengan kencang."
+                    , "Gunakan sisa tali dan ulangi proses tersebut, termasuk di bagian bawah pot."
+                    , "Pindahkan tanaman yang sudah Anda pilih ke dalam pot."
+                    , "Tanamanmu sudah jadi! Gantung di balkon atau taman Anda."},
+
+            {"Siapkan alat dan bahan yang diperlukan."
+                    , "Potong tutup botol wine menjadi dua bagian."
+                    , "Kemudian tempelkan tutup botol yang sudah dipotong pada karton."
+                    , "Atur dan susun karena tutup botol wine tidak sama persis."
+                    , "Jika Anda memiliki bentuk tertentu yang diinginkan, sebarkan lem di atas karton terlebih dahulu baru tempelkan tutup botol wine."
+                    , "Tatakan gelas telah selesai dan siap digunakan."},
+
+            {"Pertama siapkan dahulu semua bahan dan alat yang akan digunakan. Seperti batu kali aneka bentuk dan ukuran, beberapa buah pot mini, media tanam buatan  atau bisa juga diganti dengan pasir atau kerikil. Lalu siapkan juga kuas lukis dan juga beberapa cat warna hijau dan putih."
+                    , "Selanjutnya cuci semua batu kali yang akan digunakan, lalu keringkan."
+                    , "Kemudian warnai batu kali dengan aneka cat warna hijau, tunggu hingga mengering."
+                    , "Setelah batu dicat warna hijau, hiaslah batu hijau tersebut dengan hiasan duri kaktus dengan cat warna putih."
+                    , "Isilah pot bunganya dengan media tanam dan letakkan juga kaktus batu lukis yang sudah jadi ke dalamnya. Atur dan tata sedemikian rupa agar tampak bagus dan menarik."
+                    , "Kerajinan tangan dari batu sudah selesai dan bisa diletakkan sebagai hiasan ruangan."},
+
+            {"Lampu hias dari bambu harus memakai bambu yang memiliki diameter yang cukup besar."
+                    , "Masukkan lampu dengan carangnya, lalu tempelkanlah pada penutup karet."
+                    , "Pastikan lampu yang digunakan harus masuk/cukup ke dalam diameter batang bambu."
+                    , "Gunakan tali tambang untuk mengeratkan tempelan karet itu."
+                    , "Tarik kabel lampu dan gantungkan pada ruangan yang tidak memiliki lampu."},
+
+            {"Sediakan beberapa batang bambu untuk dijadikan tempat lilin."
+                    , "Potonglah batang bambu setinggi yang pas untuk lilin agar bisa tertutupi seluruhnya."
+                    , "Tempelkan lilin yang telah dimasukkan ke dalam batang bambu tersebut dengan lem tembak."
+                    , "Lubangi bagian atas bambu lalu tarik ujung sumbu lilin yang telah dimasukkan"
+                    , "Tempat lilin pun telah siap digunakan."},
+
+            {"Buatlah pola di atas kertas, misalnya saja membuat pola beruang dan juga pakaiannya. Lalu kemudian gunting pola tersebut."
+                    , "Potong 2 lembar kain flanel mengikuti pola badan beruang. Potong juga mata dan hidung dengan flanel cokelat tua."
+                    , "Tempelkan mata dan hidung beruang pada selembar potongan badan beruang. Anda dapat menjahit mata dan beruang dengan benang sulam di bagian kepala beruang. Jangan lupa untuk membuat mulut beruang dengan teknik jahit sederhana menggunakan benang."
+                    , "Kemudian, tempel keempat kancing ke potongan beruang yang sama dengan mata dan hidung tadi. Jahit 2 kancing masing-masing di sisi atas lengan, dan 2 lagi di sisi atas kaki."
+                    , "Setelah sisi depan beruang siap, mulailah menjahit kedua potongan badan beruang. Gunakan jarum pentul atau jepitan untuk menahan kedua potongan beruang agar lebih mudah dijahit. Jahit 2 lembar potongan badan beruang dibagian pinggirnya. Sisakan sedikit lubang untuk tempat mengisi isian boneka."
+                    , "Isi badan boneka dengan isian yang telah disiapkan tadi. Gunakan potongan-potongan kecil kapas atau dacorn untuk mengisi boneka dari kaki sampai kepala, jangan lupa untuk mengisi bagian telinga. "
+                    , "Selanjutnya adalah membuat pakaian beruang. Gunakan pola yang ada untuk memotong pakaian. Lipat flanel menjadi dua untuk memotong celah lubang kancing. Pakaian bisa sesederhana bentuk dasar pola dengan lubang kancing atau Anda dapat menambahkan hiasan tambahan."
+                    , "Setelah pakaian beruang jadi, pasangkan pakaian tersebut dengan kancing yang ada. Pakaian dapat dilepas dan diganti dengan pakaian lain. Anda dan anak juga dapat terus berkreasi dengan pakaian-pakaian yang ada."},
+
+            {"Gambar pola gantungan yang Anda inginkan dengan bantuan pensil atau spidol pada kain flanel."
+                    , "Sesudah sketsanya jadi, gunting kain flanel tersebut dan lakukan hal yang sama ke sisi kain flanel lainnya. Kini Anda memiliki dua bentuk kain flanel yang sama dengan pola flanel awal yang tadi. Anda dapat memadukan warnanya jika tidak ingin hanya satu warna saja."
+                    , "Langkah selanjutnya ialah gabungkan kedua lembar tersebut dan jahitlah beberapa sisinya terlebih dahulu."
+                    , "Isilah dua lembar flanel yang sudah dijahit tadi dengan dracon lalu kemudian jahit bagian yang masih terbuka agar dacron tidak keluar."
+                    , "Berikan hiasan seperti manik-manik untuk mempercantik gantungan kunci Anda."
+                    , "Gunakan lem tembak untuk memasang gantungan kunci atau menjahitnya. Gantungan kunci karya Anda telah siap diberikan pada orang terkasih."},
 
             {"Sebelum memulai, terlebih dahulu kita siapkan dahulu semua alat dan bahannya."
                     , "Potong semua sedotan yang telah disiapkan dengan ukuran sesuai selera. Berikutnya susunlah potongan sedotan membentuk bagian-bagian bangun segi enam. Untuk membantu menyambung potongan sedotannya, gunakan kawat dengan cara dimasukkan ke dalam sedotan lalu dirangkai."
@@ -47,10 +102,21 @@ public class ProductData {
                     ,"Setelah kap lampu selesai, tinggal direkatkan dengan frame yang telah kita buat sebelumnya."},
 
             {"Potong bagian bawah dari dua botol plastik dengan gunting tajam."
-                    ,"Tempatkan ritsliting di bagian tepi atas salah satu botol dengan sisi tarik ritsliting menghadap keluar. Gunakan lem panas untuk merekatkannya di tempat. Terus lem sampai di ujung ritsleting."
+                    ,"Tempatkan ritsleting di bagian tepi atas salah satu botol dengan sisi tarik ritsleting menghadap keluar. Gunakan lem panas untuk merekatkannya di tempat. Terus lem sampai di ujung ritsleting."
                     ,"Buka ritsleting sehingga Anda bisa merekatkan bagian botol lainnya."
                     ,"Rekatkan sisi lain dari ritsleting di dalam botol ke dua menggunakan lem panas. Ingatlah untuk menyisakan setidaknya 1/8 ici dari tepi botol ke gigi ritsleting."
                     ,"Sekarang kumpulkan semua barang yang berserakan dan masukkan masing-masing ke tempat khusus mereka sendiri. Anda akan sangat senang melakukannya."},
+
+            {"Siapkan bahan dan peralatan yang diperlukan."
+                    , "Ukir kaleng dengan alat ukir. Hal ini memerlukan keahlian khusus. "
+                    , "Setelas selesai mengukir, cat kaleng agar terlihat lebih indah."
+                    , "Tutup lampu dari kaleng bekas siap untuk digunakan."},
+
+            {"Siapkan koran bekas dalam jumlah banyak."
+                    , "Potong memanjang koran bekas dengan ukuran yang sama."
+                    , "Lipat memanjang potongan koran tersebut."
+                    , "Susun lipatan koran seperti menganyam hingga berbentuk keranjang."
+                    , "Agar lebih kuat, ikat bagian ujung atas keranjang dengan menggunakan tali."},
 
             {"Gunting kardus sesuai ukuran yang kamu inginkan. Buatlah dua potongan yaitu bagian depan dan belakang bingkai foto. Setelah memotong kardusnya, kamu rapihkan potongan kardus dan tentukan ukuran bingkai foto yang akan kamu buat."
                     ,"Lubangi bagian tengah kardus supaya menjadi sebuah bingkai untuk menampilkan foto yang anda taruh nanti."
@@ -59,13 +125,22 @@ public class ProductData {
                     ,"Langkah terakhir dalam cara membuat bingkai foto dari kardus adalah dengan membuat dudukan atau sanggahan. Tempelkan dudukan tersebut di cover penutup pada bagian belakang."}
     };
     public static String[][] material = new String[][]{
-            {"Gunting","Cutter","Penggaris","Lem","Manik-manik","Spidol","Cat warna"},
-            {"Sedotan", "Kawat", "Kain flanel", "Tang potong", "Gunting"},
-            {"Karton susu bekas ukuran 1 liter", "Cutter", "Lem tembak","Kain perca", "Pot kecil berisikan tanaman pilihan anda"},
-            {"Gunting", "Jarum", "Benang wol", "Sedotan"},
-            {"Kardus bekas","Lem tembak","Penggaris","Pensil","Cat semprot"},
-            {"Botol plastik bening dengan ukuran yang sama.","Gunting","Ritsleting yang sesuai dengan ukuran botol.","Lem panas tembak (hot glue gun)."},
-            {"Gunting","Cutter","Penggaris","Lem","Manik-manik","Spidol","Cat warna"}
+
+            {"Kaleng bekas (1 buah)", "Tanaman (1 buah)", "Pilok (secukupnya)", "Air panas (secukupnya)", "Baking soda (secukupnya)", "Tali (secukupnya)"},
+            {"Tutup botol wine (secukupnya)", "Karton (secukupnya)", "Cutter (1 buah)", "Lem (secukupnya)"},
+            {"Batu kali (secukupnya)", "Pot bunga mini (1 buah)", "Media tanam atau kerikil (secukupnya)", "Kuas lukis (1 buah)", "Cat (secukupnya)"},
+            {"Bambu besar (secukupnya)", "Tali tambang (1 meter)", "Lampu lonjong (1 buah)", "Penutup karet (secukupnya)"},
+            {"Batang bambu (secukupnya)", "Lilin (secukupnya)", "Lem tembak (secukupnya)"},
+            {"Kain flanel aneka warna (secukupnya)", "Kancing (secukupnya)", "Dacron/kapas (secukupnya)", "Gunting (1 buah)", "Jarum (1 buah)", "Benang sulam (secukupnya)", "Jarum pentul/klip (5 buah)", "Kertas dan pensil (1 buah)"},
+            {"Kain flanel warna berbeda (secukupnya)", "Pensil/spidol (1 buah)", "Gunting/cutter (1 buah)", "Lem tembak (secukupnya)", "Gantungan kunci (secukupnya)", "Dacron (secukupnya)", "Manik-manik (secukupnya)"},
+            {"Sedotan (secukupnya)", "Kawat (4 meter)", "Kain flanel (secukupnya)", "Tang potong (1 buah)", "Gunting (1 buah)"},
+            {"Karton susu bekas ukuran 1 liter (2 buah)", "Cutter (1 buah)", "Lem tembak (secukupnya)","Kain perca (secukupnya)", "Pot kecil berisikan tanaman pilihan anda (2 buah)"},
+            {"Gunting (1 buah)", "Jarum (1 buah)", "Benang wol (secukupnya)", "Sedotan (secukupnya)"},
+            {"Kardus bekas (1 buah)","Lem tembak (secukupnya)","Penggaris (1 buah)","Pensil (1 buah)","Cat semprot (secukupnya)"},
+            {"Botol plastik bening dengan ukuran yang sama (2 buah)","Gunting/cutter (1 buah)","Ritsleting yang sesuai dengan ukuran botol (secukupnya)","Lem tembak (secukupnya)"},
+            {"Kaleng bekas (1 buah)", "Alat ukir untuk kaleng(1 buah)", "Cat (secukupnya)", "Kuas (1 buah)"},
+            {"Koran bekas (secukupnya)", "Gunting (1 buah)", "Tali (secukupnya)"},
+            {"Gunting/cutter (1 buah)","Penggaris (1 buah)","Lem (secukupnya)","Manik-manik (secukupnya)","Spidol (1 buah)","Cat warna (secukupnya)"}
     };
 
     public static ArrayList<Product> getListData(){
